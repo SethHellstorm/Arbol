6437e1f8e35a96ad36e66d2cea8b30484ab1c60f
Rafael Dorado <Caoz01@live.com.mx>
2024-07-08 21:14:24 -0600

    lol

--- ABinario.Designer.cs
+++ ABinario.Designer.cs
@@ -159,6 +159,7 @@
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = Properties.Resources.shadowtree;
             ClientSize = new Size(800, 450);
             Controls.Add(resultados);
             Controls.Add(postOrden);
